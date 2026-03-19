6706515f77ce54461fcf98d92d1b777c361d2271
hema-chandiran07 <hemarajakumar07@gmail.com>
2026-03-19 16:56:28 +0530

    payments module update and test completed

--- nearzro-backend/src/payments/payments.module.ts
+++ nearzro-backend/src/payments/payments.module.ts
@@ -1,13 +1,29 @@
-import { Module } from '@nestjs/common';
+// src/payments/payments.module.ts
+import { Module, forwardRef } from '@nestjs/common';
+import { ScheduleModule } from '@nestjs/schedule';
 import { PaymentsService } from './payments.service';
 import { PaymentsController } from './payments.controller';
-import { RazorpayProvider } from './razorpay.provider'
+import { RazorpayProvider } from './razorpay.provider';
+import { RazorpayWebhookController } from './webhooks/razorpay-webhook.controller';
+import { PaymentReconciliationJob } from './jobs/payment-reconciliation.job';
 import { PrismaModule } from '../prisma/prisma.module';
+import { CartModule } from '../cart/cart.module';
 
 @Module({
-  imports: [PrismaModule],
-  controllers: [PaymentsController],
-  providers: [PaymentsService, RazorpayProvider],
+  imports: [
+    PrismaModule,
+    forwardRef(() => CartModule),
+    ScheduleModule.forRoot(),
+  ],
+  controllers: [
+    PaymentsController, 
+    RazorpayWebhookController
+  ],
+  providers: [
+    PaymentsService, 
+    RazorpayProvider,
+    PaymentReconciliationJob,
+  ],
   exports: [PaymentsService],
 })
 export class PaymentsModule {}
